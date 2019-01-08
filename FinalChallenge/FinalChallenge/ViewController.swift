cdbd081f4a4ef43e37d8e98cbb63758ec8fbfc48
Kensen Tjoa <kensen.tjoa@gmail.com>
2019-01-08 13:48:12 +0700

    Initial commit

---
+++ FinalChallenge/FinalChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalChallenge
+//
+//  Created by Kensen Tjoa on 08/01/19.
+//  Copyright © 2019 Kensen Tjoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
